7d90b3aaa0b78485d3ddb5e0fa974020c75b2af9
phalani <phalanisegu.1214@gmail.com>
2024-07-10 11:10:19 +0530

    My First Commit

---
+++ src/main/java/com/boa/app/BoaSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.boa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoaSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoaSpringJpaApplication.class, args);
+	}
+
+}
